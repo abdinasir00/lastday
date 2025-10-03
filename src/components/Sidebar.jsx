5afd799fe28f4539be6a2e4537b1f2dcb5b8d3fc
jamila <jamiska07@gmail>
2025-10-03 14:12:43 +0300

    Fix Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,44 +1,45 @@
-
 import { Link } from "react-router-dom"
-import { User, Plus, Bell, Search, Home  } from "lucide-react"
+import { User, Plus, Bell, Search, Home } from "lucide-react"
 
 const Sidebar = () => {
-    return (
-      
-        <div className="flex ml-12 px-16 my-8">
-         <div className="text justify-between items-center">
-         <Link to="/" className="flex items-center space-x-2">
+  return (
+    <>
+      {/* Desktop Sidebar */}
+      <div className="flex ml-12 px-16 my-8">
+        <div className="text justify-between items-center">
+          <Link to="/" className="flex items-center space-x-2">
             <Home className="h-6 w-6 text-gray" />
             <button className="hover:bg-blue-600 rounded-md font-medium w-30 py-2 hover:text-white">Home</button>
           </Link>
 
-        <Link to="/search" className="flex items-center space-x-2">
-          <Search className="h-6 w-6 text-gray-600" />
-          <button className="hover:bg-blue-600 rounded-md font-medium w-30 px-3 py-2 hover:text-white">
-            Search
-          </button>
-        </Link>
+          <Link to="/search" className="flex items-center space-x-2">
+            <Search className="h-6 w-6 text-gray-600" />
+            <button className="hover:bg-blue-600 rounded-md font-medium w-30 px-3 py-2 hover:text-white">
+              Search
+            </button>
+          </Link>
 
-        <Link to="/create" className="flex items-center space-x-2">
-          <Plus className="h-6 w-6 text-gray-600" />
-          <button className="hover:bg-blue-600 rounded-md font-medium w-30 px-3 py-2 hover:text-white">
-            Create
-          </button>
-        </Link>
+          <Link to="/create" className="flex items-center space-x-2">
+            <Plus className="h-6 w-6 text-gray-600" />
+            <button className="hover:bg-blue-600 rounded-md font-medium w-30 px-3 py-2 hover:text-white">
+              Create
+            </button>
+          </Link>
 
-        <Link to="/notifications" className="flex items-center space-x-2">
-          <Bell className="h-6 w-6 text-gray-600" />
-          <button className="hover:bg-blue-600 rounded-md font-medium w-30 px-3 py-2 hover:text-white">
-            Notifications
-          </button>
-        </Link>
+          <Link to="/notifications" className="flex items-center space-x-2">
+            <Bell className="h-6 w-6 text-gray-600" />
+            <button className="hover:bg-blue-600 rounded-md font-medium w-30 px-3 py-2 hover:text-white">
+              Notifications
+            </button>
+          </Link>
 
-        <Link to="/profile" className="flex items-center space-x-2">
-          <User className="h-6 w-6 text-gray-600" />
-          <button className="hover:bg-blue-600 rounded-md w-30 font-medium px-3 py-2 hover:text-white">
-            Profile
-          </button>
-        </Link>
+          <Link to="/profile" className="flex items-center space-x-2">
+            <User className="h-6 w-6 text-gray-600" />
+            <button className="hover:bg-blue-600 rounded-md w-30 font-medium px-3 py-2 hover:text-white">
+              Profile
+            </button>
+          </Link>
+        </div>
       </div>
 
       {/* Mobile Bottom Navbar */}
@@ -69,16 +70,7 @@ const Sidebar = () => {
         </Link>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Sidebar;
-
-
-
-
-
-
-
-
-
+export default Sidebar
